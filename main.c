f6ace9e13f4565dac6069ca6070b2322a031f973
Steve(laptop.H) <stevenalexcarino@gmail.com>
2019-07-30 17:02:33 -0400

    placeholder plan for functions

---
+++ main.c
@@ -0,0 +1,20 @@
+// Mortuary Management System //
+// Shawn, Steve Carino, Casey Byrne
+
+//DATA STRUCTS
+//bodies
+
+//stack (using stack so we can use depth first traversal)
+
+//FUNCTIONS
+
+//addRecord
+//editRecord
+//deleteRecord
+//search
+
+//readCSV
+//writeCSV
+//quickSort
+//DFTSearch
+
