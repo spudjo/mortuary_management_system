42915a26fae9c682809bf1787cc444b1de3e6806
Shawn P <spudjo@protonmail.com>
2019-08-02 14:40:27 -0400

    Update main_b.c
    - Data Structure, Add, Update complete
    - Needs exception handling and validation

--- main_b.c
+++ main_b.c
@@ -19,7 +19,22 @@
 #include <string.h>
 #include <time.h>
 
-const int MAX_CAPACITY = 100;
+const int MAX_MORGUE_CAPACITY = 1000;
+const int MAX_STRING_CAPACITY = 100;
+
+char* remove_newline(char *string)
+{
+    int x;
+    for(x = 0; x <= MAX_STRING_CAPACITY; x++)
+    {
+        if( string[x] == '\n')
+        {
+            string[x] = '\0';
+            break;
+        }
+    }
+    return string;
+}
 
 typedef struct date
 {
@@ -55,21 +70,31 @@ DatePtr create_date(int year, int month, int day)
 /*
  * Create and return BodyPtr
  */
-Body create_body(int id, char name[50], char sex, int age, int year, int month, int day, double weight, double height, char cause_of_death[50])
+Body create_body(int id, char name[], char sex, int age, 
+                 int year, int month, int day, 
+                 double weight, double height, char cause_of_death[])
 {       
     Body body;
     body.id = id;
-    body.name = (char*)calloc(strlen(name), sizeof(char));
-    strcpy (body.name, name);
+    strcpy(body.name, name);
     body.sex = sex;
     body.age = age;  
     body.date_of_death = create_date(year, month, day);  
     body.weight = weight;
     body.height = height;
-    body.cause_of_death = (char*)calloc(strlen(cause_of_death), sizeof(char));
-    strcpy (body.cause_of_death, cause_of_death);
+    strcpy(body.cause_of_death, cause_of_death);
     return body;
 }
+
+Body create_body_empty(int id)
+{
+    Body body;
+    body.id = id;
+    body.name = calloc(MAX_MORGUE_CAPACITY, sizeof(char));
+    body.cause_of_death = calloc(MAX_CAPACITY, sizeof(char));
+    return body;
+}
+
 
 /*
  * Prints of DatePtr contents in YYYY-MM-DD format
@@ -77,7 +102,7 @@ Body create_body(int id, char name[50], char sex, int age, int year, int month, 
  */
 void print_date(DatePtr date)
 {
-    printf("%d-%02d-%02d", date->year, date->month, date->day);
+    printf("        | Date of Death: %d-%02d-%02d | ", date->year, date->month, date->day);
 }
 
 /*
@@ -85,219 +110,218 @@ void print_date(DatePtr date)
  */
 void print_body_info(Body body)
 {
-    printf("%d | ", body.id);
-    
-    for (int i = 0; i < strlen(body.name); i++)
-    {
-        printf("%c", body.name[i]);
-    }
-    
-    printf(" | %c | %d | ", body.sex, body.age);  
-    
+    printf("ID: %05d | Name: %s\n", body.id, body.name);
+    printf("        | Sex: %c | Age: %02d | Weight: %03.2lf KG | Height: %03.2lf CM\n", 
+            body.sex, body.age, body.weight, body.height);
     print_date(body.date_of_death);
-    
-    printf(" | %.2lf kg | %.2lf cm| ", body.weight, body.height); 
-    
-    for (int i = 0; i < strlen(body.cause_of_death); i++)
-    {
-        printf("%c", body.cause_of_death[i]);
-    }
-    printf("\n");
+    printf("Cause of Death: %s\n", body.cause_of_death); 
 }
 
+/*
+ * Print info on all bodies in collection
+ */
 void print_body_collection(BodyPtr body_collection)
 {
-    for (int i = 0; i < MAX_CAPACITY; i++)
+    printf("\n=================================================================\n");
+    printf("=                 B O D Y - C O L L E C T I O N                 =");
+    printf("\n=================================================================\n");
+    for (int i = 0; i < MAX_MORGUE_CAPACITY; i++)
     {
         if (body_collection[i].name != NULL)
         {
-            printf("Body found at %d : ID = ", i);
             print_body_info(body_collection[i]);
         }
     }
+    printf("=================================================================\n");
 }
 
-BodyPtr create_body_collect(int size)
+/*
+ * Allocate space in heap for body_collection
+ */
+BodyPtr create_body_collection(int size)
 {
-    BodyPtr body_collection = (BodyPtr*)malloc(size * sizeof(Body));
+    BodyPtr body_collection = (BodyPtr*)calloc(size, sizeof(Body));
     return body_collection;
 }
 
 /*
  * Hash Function
  */
-BodyPtr add_to_collection(BodyPtr body_collection, Body body)
+void add_to_collection(BodyPtr body_collection, Body body)
 {
     body_collection[body.id] = body;
 }
 
-/*
-    int id;
-    char* name;
-    char sex;
-    int age;
-    DatePtr date_of_death;
-    double weight;
-    double height;
-    char* cause_of_death; 
- 
- */
-
-/* Functions to updating Body fields*/
-//
-//void update_body_id(BodyPtr body_collection, int id, int new_int)
-//{
-//    body_collection[id].id = new_int;
-//}
-
-void update_body_name(BodyPtr body_collection, int id)
+void set_body_name(BodyPtr body_collection, int id)
 {
     printf("Enter Name: ");
-    char* new_name[50];
-    scanf("%s", new_name);
+    char *name;
+    fgets(name, MAX_STRING_CAPACITY, stdin); 
     fflush(stdin);
-    strcpy(body_collection[id].name, new_name);    
+    name = remove_newline(name);
+//    printf("Variable: \n%s\n", name);
+//    body_collection[id].name = name;
+    strcpy(body_collection[id].name, name);
+//    printf("Name: \n%s\n", body_collection[id].name);
+//    printf("CoD: \n%s\n", body_collection[id].cause_of_death);
 }
 
-void update_body_sex(BodyPtr body_collection, int id)
+void set_body_sex(BodyPtr body_collection, int id)
 {
     printf("Enter Sex: ");
-    char new_sex;
-    scanf("%s", &new_sex);
-    fflush(stdin);
-    body_collection[id].sex = new_sex;
+    char sex = getchar();
+    getchar();
+    body_collection[id].sex = sex;
 }
 
-void update_body_age(BodyPtr body_collection, int id)
+void set_body_age(BodyPtr body_collection, int id)
 {
-    printf("Enter Age: ");
-    int new_age;
-    scanf("%d", &new_age);
-    fflush(stdin);
-    body_collection[id].age = new_age;
+    printf("Enter Age: ");   
+    int age;
+    scanf("%d", &age);
+    getchar();
+    body_collection[id].age = age;
 }
 
-void update_body_date_of_death(BodyPtr body_collection, int id)
+void set_body_date_of_death(BodyPtr body_collection, int id)
 {
     printf("Enter Date of Death (Format: YYYY MM DD): ");
     int year, month, day;
     scanf("%d %d %d", &year, &month, &day);
-    fflush(stdin);
+    getchar();
     body_collection[id].date_of_death = create_date(year, month, day);   
 }
 
-void update_body_weight(BodyPtr body_collection, int id)
+void set_body_weight(BodyPtr body_collection, int id)
 {
     printf("Enter Weight: ");
-    int new_weight;
-    scanf("%d", &new_weight);
-    fflush(stdin); 
-    body_collection[id].weight = new_weight;
+    double weight;
+    scanf("%lf", &weight);
+    getchar();
+    body_collection[id].weight = weight;
 }
 
-void update_body_height(BodyPtr body_collection, int id)
+void set_body_height(BodyPtr body_collection, int id)
 {
     printf("Enter Height: ");
-    int new_height;
-    scanf("%d", &new_height);
-    fflush(stdin); 
-    body_collection[id].height = new_height;
+    double height;
+    scanf("%lf", &height);
+    getchar(); 
+    body_collection[id].height = height;
 }
 
-void update_body_cause_of_death(BodyPtr body_collection, int id)
+void set_body_cause_of_death(BodyPtr body_collection, int id)
 {
     printf("Enter Cause of Death: ");
-    char* new_cause_of_death[50];
-    scanf("%s", new_cause_of_death);
-    fflush(stdin);  
-    strcpy(body_collection[id].cause_of_death, new_cause_of_death);  
+    char *cause_of_death;
+    fgets(cause_of_death, MAX_STRING_CAPACITY, stdin); 
+    fflush(stdin);
+    cause_of_death = remove_newline(cause_of_death);
+//    printf("Variable: \n%s\n", cause_of_death);
+//    body_collection[id].cause_of_death = cause_of_death;
+    strcpy(body_collection[id].cause_of_death, cause_of_death);
+//    printf("Name: \n%s\n", body_collection[id].name);
+//    printf("CoD: \n%s\n", body_collection[id].cause_of_death);
 }
 
 void update_body(BodyPtr body_collection)
 {
+    printf("\n=================================================================\n");
+    printf("=                     E D I T - R E C O R D                     =");
+    printf("\n=================================================================\n");
     printf("Enter ID of body to update: ");
     int id;
     scanf("%d", &id);
-    fflush(stdin);
+    getchar();
     
-    int id_exists = validate_existing_id(body_collection);
-    
+    // checks if ID exists in collection
+    int id_exists = validate_existing_id(body_collection, id);
     if (id_exists == 0)
     {
-        printf("No body exists with that ID");
+        printf("ID not found!\n");
         return;
     }
-    printf("Editing Body %d\n", id);
-    printf("Which value to edit?\n");
-    printf("0: Name | 1: Sex | 2: Age | 3: Date of death | 4: Weight | 5: Height | 6: Cause of death\n");
     
-    int change_value;
-    scanf("%d", &change_value);
-    fflush(stdin);
-    
-    switch(change_value){
-		case 0:
-                    update_body_name(body_collection, id);
-                    break;
-		case 1:
-                    update_body_sex(body_collection, id);
-                    break;
-		case 2:
-                    update_body_age(body_collection, id);
-                    break;
-		case 3:
-                    update_body_date_of_death(body_collection, id);
-                    break;
-		case 4:
-                    update_body_weight(body_collection, id);
-                    break;
-                case 5:
-                    update_body_height(body_collection, id);
-                    break;
-                case 6:
-                    update_body_cause_of_death(body_collection, id);
-                    break;		
+    char select;
+    while (select != 'q')
+    {
+        printf("Editing Record ID %d\n\n", id);
+        printf("Select a value:\n");
+        printf("Name: n   |   Sex: s   |   Age: a   |   Weight: w   |   Height: h\n");
+        printf("Date of death: d   |   Cause of death: c\n");
+        printf("Quit: q\n\n");
+        printf("Selection: ");
+ 
+        select = getchar();
+        getchar();
+        printf("\n");
+        
+        switch(select){
+            case 'n':
+                set_body_name(body_collection, id);
+                break;
+            case 's':
+                set_body_sex(body_collection, id);
+                break;
+            case 'a':
+                set_body_age(body_collection, id);
+                break;
+            case 'd':
+                set_body_date_of_death(body_collection, id);
+                break;
+            case 'w':
+                set_body_weight(body_collection, id);
+                break;
+            case 'h':
+                set_body_height(body_collection, id);
+                break;
+            case 'c':
+                set_body_cause_of_death(body_collection, id);
+                break;		
 	}
+        printf("\n");
+        print_body_info(body_collection[id]);
+        printf("\n");
+    }  
 }
 
 void add_body(BodyPtr body_collection)
 {
+    printf("\n=================================================================\n");
+    printf("=                      A D D - R E C O R D                      =");
+    printf("\n=================================================================\n");
     printf("Enter ID of new body: ");
     int id;
     scanf("%d", &id);
-    fflush(stdin);
+    getchar();
     
-    int id_exists = validate_existing_id(body_collection);
-    
+    int id_exists = validate_existing_id(body_collection, id);
     if (id_exists == 1)
     {
         printf("ID is currently being used");
         return;
     }
     
-    Body body;
-    body.id = id;
-    update_body_name(body_collection, id);
-    update_body_sex(body_collection, id);
-    update_body_age(body_collection, id);
-    update_body_date_of_death(body_collection, id);
-    update_body_weight(body_collection, id);
-    update_body_height(body_collection, id);
-    update_body_cause_of_death(body_collection, id);	  
+    Body body = create_body_empty(id);
+    add_to_collection(body_collection, body);
+    set_body_name(body_collection, id);
+    set_body_sex(body_collection, id);
+    set_body_age(body_collection, id);
+    set_body_weight(body_collection, id);
+    set_body_height(body_collection, id);
+    set_body_date_of_death(body_collection, id);
+    set_body_cause_of_death(body_collection, id);	  
 }
 
 /*
  * Checks if an ID is currently being used 
  * If so, return true, else return false indicating the ID is available
  */
-int validate_existing_id(BodyPtr body_collection)
+int validate_existing_id(BodyPtr body_collection, int id)
 {
-    for (int i = 0; i < MAX_CAPACITY; i++)
+    if (body_collection[id].name != NULL)
     {
-        if (body_collection[i].name != NULL)
-        {
-            return 1;
-        }
+        return 1;
     }
     return 0;
 }
@@ -308,14 +332,14 @@ int validate_existing_id(BodyPtr body_collection)
 int main(int argc, char** argv) {
     
     // array of Body structs
-    BodyPtr body_collection = create_body_collect(MAX_CAPACITY);
-//    Body body_A = create_body(5, "Casey Byrne", 'M', 27, 2019, 8, 01, 56.5, 176.5, "Autoerotic Asphyxiation");
-//    add_to_collection(body_collection, body_A);  
+    BodyPtr body_collection = create_body_collection(MAX_MORGUE_CAPACITY);
     add_body(body_collection);
     print_body_collection(body_collection);
     add_body(body_collection);
     print_body_collection(body_collection);
     add_body(body_collection);
+    print_body_collection(body_collection);
+    update_body(body_collection);
     print_body_collection(body_collection);
     update_body(body_collection);
     print_body_collection(body_collection);
